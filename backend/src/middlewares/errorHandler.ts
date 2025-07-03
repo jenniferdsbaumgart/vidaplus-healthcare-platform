bb30007d7ae0681409d4c5bf650092f7551a91a5
Jennifer Baumgart <jenniferdsbaumgart@gmail.com>
2025-07-02 22:56:19 -0300

    feat: enhance error handler to include NextFunction parameter

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,9 +1,11 @@
-import { Request, Response } from 'express';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response, NextFunction } from 'express';
 
 export function errorHandler(
   err: Error & { status?: number },
   req: Request,
   res: Response,
+  next: NextFunction
 ) {
   console.error(err);
   res.status(err.status || 500).json({
